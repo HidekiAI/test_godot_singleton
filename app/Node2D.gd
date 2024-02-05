ca986fb8407a94cf9bc6d39e447e6a29a776bed2
Hideki A. Ikeda <hidekiai+hidekiai-lt@CodeMonkeyNinja.dev@users.noreply.github.com>
2024-02-05 13:02:18 -0600

    though "new()" shows up in Godot Script editor intellisense, at least it is now working thanks to folks at discord-gdext

--- app/Node2D.gd
+++ app/Node2D.gd
@@ -1,14 +1,11 @@
 extends Node
 
 func _ready() -> void:
-	# Singleton seems to be broken or I am using this wrong...  Documentation says to do:
-	# MyEditorSingleton.foo() without new()
+	MyEditorSingleton.foo(); # without new()
+	var v = MyEditorSingleton.foo_value;
+	print("foo_value = " + str(v))
 	
 	# Following will new 3 times, hence each call to X_foo() will be a new instance...
-	MyEditorSingleton.new().get_foo()
-	MyEditorSingleton.new().set_foo(42)	# this does NOT seem right...
-	MyEditorSingleton.new().get_foo()
-
-func _process(delta):
-	var v = MyEditorSingleton.new().get_foo();
-	print("static value = " + str(v) );
+	MyEditorSingleton.get_foo();
+	MyEditorSingleton.set_foo(42);	# this does NOT seem right...
+	MyEditorSingleton.get_foo();
--- libs/src/lib.rs
+++ libs/src/lib.rs
@@ -6,10 +6,6 @@ use godot::{engine::Engine, prelude::*};
 // NOTE: The code is hybrid logic from  https://godot-rust.github.io/book/recipes/engine-singleton.html
 // sample code godot-rust book, and have been modified to make it usable.
 
-// NOT in documentation, but needed because Godot editor will not find MyEditorSingleton otherwise...
-#[gdextension]
-unsafe impl ExtensionLibrary for MyEditorSingleton {}
-
 #[derive(GodotClass)]
 //#[class(tool, init, base=Object)] // uncomment this (with default init) and remove below (impl init)
 #[class(tool, base=Object)]
@@ -44,6 +40,11 @@ impl MyEditorSingleton {
     //}
 
     #[func]
+    fn foo(&mut self) {
+        godot_print!("MyEditorSingleton::foo() - foo={:?}", self.foo_value);
+    }
+
+    #[func]
     fn set_foo(&mut self, v: i64) {
         godot_print!("MyEditorSingleton::set_foo({:?})", v);
         self.foo_value = v;
@@ -59,65 +60,75 @@ impl MyEditorSingleton {
 //}
 
 ///////////////////////////////////////////////////////////////////// MyExtension
-pub struct MyExtension {}
+pub mod entry_point {
+    use godot::{engine::Engine, prelude::*};
+    use godot::obj::cap::GodotDefault;
 
-unsafe impl ExtensionLibrary for MyExtension {
-    fn on_level_init(level: InitLevel) {
-        let singleton_struct = StringName::from("MyEditorSingleton");
-        if level == InitLevel::Scene {
-            // The StringName identifies your singleton and can be
-            // used later to access it.
-            Engine::singleton()
-                .register_singleton(singleton_struct, MyEditorSingleton::new_alloc().upcast());
-        }
-    }
+    #[derive(GodotClass)]
+    #[class(base=Object)]
+    pub struct MyExtension {}
 
-    fn on_level_deinit(level: InitLevel) {
-        let singleton_struct = StringName::from("MyEditorSingleton");
-        if level == InitLevel::Scene {
-            // Unregistering is needed to avoid memory leaks and
-            // warnings, especially for hot reloading.
-            Engine::singleton().unregister_singleton(singleton_struct);
-        }
-    }
-}
+    impl godot::obj::cap::GodotDefault for MyExtension {}
 
-//////////////////////////////////////////////////////////////////////// tests
-#[cfg(test)]
-mod tests {
-    // Import the function under test
-    use super::*;
-    use godot::prelude::*;
-
-    #[test]
-    fn test_get_ref() {
-        // NOTE: This all asserts will ALWAYS fail/assert because Godot Engine is not going to be running, it's only here
-        // as a means of completness to the usage of this extension-library
-        let singleton_struct = StringName::from("MyEditorSingleton");
-        let possible_singleton =
-            godot::engine::Engine::singleton().get_singleton(singleton_struct.clone());
-        assert!(
-            possible_singleton.is_some(),
-            "Singleton '{}' not found",
-            singleton_struct.to_string()
-        );
-        match possible_singleton {
-            Some(mut singleton) => {
-                let funcname = "foo";
-                let foo_arg1 = 42;
-                let found_func = singleton.has_method(StringName::from(funcname));
-                assert!(found_func, "Method '{}' not found", funcname);
-                let call_result =
-                    singleton.call(StringName::from(funcname), &[Variant::from(foo_arg1)]);
-                assert_eq!(
-                    call_result.is_nil(),
-                    false,
-                    "Call to '{}'({}) failed",
-                    funcname,
-                    foo_arg1
-                );
+    // define an entry point for which the singleton is registered
+    #[gdextension]
+    unsafe impl ExtensionLibrary for MyExtension {
+        fn on_level_init(level: InitLevel) {
+            let singleton_struct = StringName::from("MyEditorSingleton");
+            if level == InitLevel::Scene {
+                // The StringName identifies your singleton and can be
+                // used later to access it.
+                Engine::singleton()
+                    .register_singleton(singleton_struct, super::MyEditorSingleton::new_alloc().upcast());
             }
-            None => panic!("Singleton '{}' not found", singleton_struct.to_string()),
+        }
+
+        fn on_level_deinit(level: InitLevel) {
+            let singleton_struct = StringName::from("MyEditorSingleton");
+            if level == InitLevel::Scene {
+                // Unregistering is needed to avoid memory leaks and
+                // warnings, especially for hot reloading.
+                Engine::singleton().unregister_singleton(singleton_struct);
+            }
+        }
+    }
+
+    //////////////////////////////////////////////////////////////////////// tests
+    #[cfg(test)]
+    mod tests {
+        // Import the function under test
+        use super::*;
+
+        #[test]
+        fn test_get_ref() {
+            // NOTE: This all asserts will ALWAYS fail/assert because Godot Engine is not going to be running, it's only here
+            // as a means of completness to the usage of this extension-library
+            let singleton_struct = StringName::from("MyEditorSingleton");
+            let possible_singleton =
+                godot::engine::Engine::singleton().get_singleton(singleton_struct.clone());
+            assert!(
+                possible_singleton.is_some(),
+                "Singleton '{}' not found",
+                singleton_struct.to_string()
+            );
+            match possible_singleton {
+                Some(mut singleton) => {
+                    let funcname = "foo";
+                    let foo_arg1 = 42;
+                    let found_func = singleton.has_method(StringName::from(funcname));
+                    assert!(found_func, "Method '{}' not found", funcname);
+                    let call_result =
+                        singleton.call(StringName::from(funcname), &[Variant::from(foo_arg1)]);
+                    assert_eq!(
+                        call_result.is_nil(),
+                        false,
+                        "Call to '{}'({}) failed",
+                        funcname,
+                        foo_arg1
+                    );
+                }
+                None => panic!("Singleton '{}' not found", singleton_struct.to_string()),
+            }
         }
     }
 }
